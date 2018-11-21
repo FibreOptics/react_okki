dd31450362467407dfd709daf2e6e6701f4481ed
FibreOptics <nacalon@hotmail.com>
2018-11-21 13:54:41 +0100

    simple router

--- react_okki/src/App/pages/Home.js
+++ react_okki/src/App/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 class Home extends Component {
   render() {
     return (
@@ -12,6 +11,7 @@ class Home extends Component {
         <button variant="raised">
             My List
         </button>
+        <p>Hello</p>
       </Link>
     </div>
     );
